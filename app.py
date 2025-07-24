f775ec7f5b83c3b7a0dd50aa1f12c1735a898ffa
athuy125 <athuy12052009@gmail.com>
2025-07-24 18:10:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from zoneinfo import ZoneInfo
 import glob
 import schedule
 import time
-from db import save_data, load_data
+from db import save_data, load_data, get_history
 
 # Tạo thư mục nếu chưa có
 DATA_FOLDER = "data"
